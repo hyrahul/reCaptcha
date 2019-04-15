5812a77aad203d46ca9fb6b69184128b8e7e3883
RP <hyrahulpal@gmail.com>
2019-04-16 00:27:33 +0530

    This is my first commit

---
+++ src/blog/sodhanalibrary/CaptchaResponse.java
@@ -0,0 +1,14 @@
+package blog.sodhanalibrary;
+
+public class CaptchaResponse {
+	public boolean success;
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+}
